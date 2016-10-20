6feb71ce62cdbee97f433d13a5af8c8a0821f305
daviscoder <yunfeng.chn@gmail.com>
2016-10-19 22:38:41 -0700

    Create 398. Random Pick Index.cpp

---
+++ 398. Random Pick Index.cpp
@@ -0,0 +1,30 @@
+// O(n), O(1)
+
+class Solution {
+private:
+    vector<int> nums;
+public:
+    Solution(vector<int> nums) {
+        this->nums = nums;
+        srand(time(0));
+    }
+    
+    int pick(int target) {
+        int result = 0, count = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == target) {
+                if (rand() % count == 0) {
+                    result = i;
+                    count++;
+                }
+            }
+        }
+        return result;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int param_1 = obj.pick(target);
+ */
